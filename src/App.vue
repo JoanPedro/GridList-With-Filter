c5df6a5d083e2dfa5fa352bda338c276ea8c38dd
Joan Pedro <jpos.senai@gmail.com>
2020-03-24 12:40:44 -0300

    Finaliza Projeto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <div id="app" class="">
-      <h1> Template para Projetos Vue </h1>
+  <div id="app" class="demo">
+    <div id="demo">
+      <form id="search">
+        Search <input name="query" v-model="searchQuery" />
+      </form>
+      <GridList
+        :heroes="gridData"
+        :columns="gridColumns"
+        :filter-key="searchQuery"
+      >
+      </GridList>
+    </div>
   </div>
 </template>
 
 <script>
+import GridList from './components/GridList'
 export default {
   name: 'app',
-  components: {
+  components: {GridList},
+  data: function(){
+    return {
+      searchQuery: "",
+      gridColumns: ["name", "power"],
+      gridData: [
+        { name: "Chuck Norris", power: Infinity },
+        { name: "Bruce Lee", power: 9000 },
+        { name: "Jackie Chan", power: 7000 },
+        { name: "Jet Li", power: 8000 }
+      ]
+    }
   }
 }
 </script>
+
+<style>
+body {
+  font-family: Helvetica Neue, Arial, sans-serif;
+  font-size: 14px;
+  color: #444;
+}
+</style>
